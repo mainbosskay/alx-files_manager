2311652df8d1c3f6c66bbf335c9f281de7b70f64
mainbosskay <dadakehinde80@yahoo.com>
2024-10-01 06:44:57 +0000

    Creating express server and t should listen on the port set by the environment variable PORT or by default 5000

---
+++ controllers/UsersController.js
@@ -0,0 +1,62 @@
+import Queue from 'bull/lib/queue';
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+/* eslint-disable import/no-named-as-default */
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const queueUser = new Queue('email sending');
+
+class UsersController {
+  static async postNew(req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+    const existingUser = await (await dbClient.usersCollection()).findOne({ email });
+
+    if (existingUser) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+    const insertData = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertData.insertedId.toString();
+
+    queueUser.add({ userId });
+    res.status(201).json({ email, id: userId });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token') || null;
+    if (!token) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    const userCollection = await dbClient.usersCollection();
+    const user = await userCollection.findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    res.status(200).json({ id: userId, email: user.email });
+  }
+}
+
+export default UsersController;
